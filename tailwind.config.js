f1efabaf1e87b74f00454d72b28fd0ef99a55f81
jo <95hyouka@gmail.com>
2024-12-02 11:10:30 +0000

    npm run format

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+	theme: {
+		extend: {}
+	},
 
-  plugins: [typography]
+	plugins: [typography]
 };
